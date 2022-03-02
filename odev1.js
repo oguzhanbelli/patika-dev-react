87146a1d308d53875612232ecc2a2a8aca8a20d0
Oğuzhan Belli <oguzhanbellii@gmail.com>
2022-03-02 14:16:05 +0300

    Update users

--- odev1.js
+++ odev1.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
 const getData = async(userId) => {
-  const { data: users } = await axios(
+  const { data: posts } = await axios(
     `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
   );
+  const { data: users } = await axios(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
 
-  console.log(users);
+  let obj = {
+      ...users,
+      posts
+  };
+  
+
+  console.log(obj)
 };
 
 export  {getData as default};
